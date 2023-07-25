03ba9190019dc3ae1a5941c16bae86e095583958
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-07-25 14:32:15 +0530

    j

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [process.env.WP_IMAGES_URL],
+  },
 }
 
 module.exports = nextConfig
